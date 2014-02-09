0ae3fa35c2afecade4ad7e8bb89f9a865db1dfe1
Christopher Gunn <christopher.gunn@drllimited.co.uk>
2014-02-09 21:00:05 +0000

    Added Nancy and Nancy.Hosting.Aspnet NuGet packages. Added Game and Game.Tests projects for domain/POCO objects and tests, respectively. Tests project has NUnit and NSubstitute packages installed.

---
+++ Game.WebApp/Api/GamePlay.cs
@@ -0,0 +1,11 @@
+﻿using Nancy;
+
+namespace Game.WebApp.Api
+{
+    public class GamePlay : NancyModule
+    {
+        public GamePlay()
+        {
+            Get["/"] = _ => "Nice one.";}
+    }
+}
