b3aaed98d7464d7eaae4b8f175cafe8cea5472ec
Christopher Gunn <christopher.gunn@drllimited.co.uk>
2014-02-09 21:08:49 +0000

    Added first test of Domain object Game (creation).

---
+++ Game.Tests/Domain/GameTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Game.Tests.Domain
+{
+    [TestFixture]
+    public class GameTests
+    {
+        [Test]
+        public void CanCreateGame()
+        {
+            new Game();
+        }
+    }
+}
