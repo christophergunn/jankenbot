88f1203bec68a3085ffe1dc002b61f49b3255423
Christopher Gunn <christopher.gunn@drllimited.co.uk>
2014-02-10 19:56:12 +0000

    Implement standard rules and tests for these in Game object.

--- Game.Tests/Domain/GameTests.cs
+++ Game.Tests/Domain/GameTests.cs
@@ -15,5 +15,34 @@ namespace Game.Tests.Domain
         {
             new Game();
         }
+
+        [Test]
+        [TestCase(Move.Rock, Move.Rock, RoundResult.Neither)]
+        [TestCase(Move.Paper, Move.Paper, RoundResult.Neither)]
+        [TestCase(Move.Scissors, Move.Scissors, RoundResult.Neither)]
+        public void GivenSameMoves_NeitherPlayerWins(Move playerOneMove, Move playerTwoMove, RoundResult expectedResult)
+        {
+            var game = new Game();
+
+            game.PlayMoves(playerOneMove, playerTwoMove);
+
+            Assert.That(game.LastResult, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        [TestCase(Move.Rock, Move.Paper, RoundResult.PlayerTwoWins)]
+        [TestCase(Move.Rock, Move.Scissors, RoundResult.PlayerOneWins)]
+        [TestCase(Move.Paper, Move.Scissors, RoundResult.PlayerTwoWins)]
+        [TestCase(Move.Paper, Move.Rock, RoundResult.PlayerOneWins)]
+        [TestCase(Move.Scissors, Move.Rock, RoundResult.PlayerTwoWins)]
+        [TestCase(Move.Scissors, Move.Paper, RoundResult.PlayerOneWins)]
+        public void GivenStandardMoves_CanDetermineWinner(Move playerOneMove, Move playerTwoMove, RoundResult expectedResult)
+        {
+            var game = new Game();
+
+            game.PlayMoves(playerOneMove, playerTwoMove);
+
+            Assert.That(game.LastResult, Is.EqualTo(expectedResult));
+        }
     }
 }
--- Game/Game.cs
+++ Game/Game.cs
@@ -6,7 +6,57 @@ using System.Threading.Tasks;
 
 namespace Game
 {
+    public enum RoundResult
+    {
+        PlayerOneWins,
+        Neither,
+        PlayerTwoWins
+    }
+
+    public enum Move
+    {
+        Rock,
+        Paper,
+        Scissors
+    }
+
     public class Game
     {
+        public void PlayMoves(Move playerOneMove, Move playerTwoMove)
+        {
+            if (playerOneMove == playerTwoMove)
+            {
+                LastResult = RoundResult.Neither;
+            } 
+            else if (playerOneMove == Move.Rock && playerTwoMove == Move.Paper)
+            {
+                LastResult = RoundResult.PlayerTwoWins;
+            }
+            else if (playerOneMove == Move.Rock && playerTwoMove == Move.Scissors)
+            {
+                LastResult = RoundResult.PlayerOneWins;
+            }
+            else if (playerOneMove == Move.Paper && playerTwoMove == Move.Rock)
+            {
+                LastResult = RoundResult.PlayerOneWins;
+            }
+            else if (playerOneMove == Move.Paper && playerTwoMove == Move.Scissors)
+            {
+                LastResult = RoundResult.PlayerTwoWins;
+            }
+            else if (playerOneMove == Move.Scissors && playerTwoMove == Move.Paper)
+            {
+                LastResult = RoundResult.PlayerOneWins;
+            }
+            else if (playerOneMove == Move.Scissors && playerTwoMove == Move.Rock)
+            {
+                LastResult = RoundResult.PlayerTwoWins;
+            }
+        }
+
+        public RoundResult LastResult
+        {
+            get; private set; 
+        }
     }
 }
